704bbe6fe639e38558348f8f009ac63db8f62d47
Joseph D <j_seremba@live.com>
2018-06-20 09:48:26 +0300

    Handle validations

--- src/components/TagContainer.ts
+++ src/components/TagContainer.ts
@@ -6,20 +6,21 @@ import { ValidateConfigs } from "../utils/ValidateConfigs";
 interface WrapperProps {
     class?: string;
     mxObject: mendix.lib.MxObject;
+    mxform: mxui.lib.form._FormBase;
     readOnly: boolean;
     style?: string;
 }
 
 export interface TagContainerProps extends WrapperProps {
     afterCreateMicroflow: string;
-    allowDrag: boolean;
+    afterCreateNanoflow: Nanoflow;
     tagStyle: BootstrapStyle;
     editable: "default" | "never";
     enableSuggestions: boolean;
     inputPlaceholder: string;
     onChangeMicroflow: string;
+    onChangeNanoflow: Nanoflow;
     lazyLoad: boolean;
-    mxform: mxui.lib.form._FormBase;
     tagAttribute: string;
     tagConstraint: string;
     tagEntity: string;
@@ -38,10 +39,15 @@ export interface TagContainerState {
     tagCache: mendix.lib.MxObject[];
 }
 
+interface Nanoflow {
+    nanoflow: object[];
+    paramsSpec: { Progress: string };
+}
+
 export default class TagContainer extends Component<TagContainerProps, TagContainerState> {
     private subscriptionHandles: number[] = [];
     private tagEntity: string;
-    private referenceAttribute: string;
+    private reference: string;
 
     constructor(props: TagContainerProps) {
         super(props);
@@ -56,19 +62,15 @@ export default class TagContainer extends Component<TagContainerProps, TagContai
             tagList: []
         };
 
-        this.createTag = this.createTag.bind(this);
-        this.processTags = this.processTags.bind(this);
-        this.lazyLoadSuggestions = this.lazyLoadSuggestions.bind(this);
-        this.removeTag = this.removeTag.bind(this);
         this.tagEntity = props.tagEntity.split("/")[props.tagEntity.split("/").length - 1];
-        this.referenceAttribute = props.tagEntity.split("/")[0];
+        this.reference = props.tagEntity.split("/")[0];
     }
 
     render() {
         return createElement(Tag, {
             alertMessage: this.state.alertMessage,
             className: this.props.class,
-            createTag: this.createTag,
+            createTag: this.validateTag,
             enableSuggestions: this.props.enableSuggestions,
             fetchSuggestions: this.lazyLoadSuggestions,
             inputPlaceholder: this.props.inputPlaceholder,
@@ -86,7 +88,7 @@ export default class TagContainer extends Component<TagContainerProps, TagContai
     }
 
     componentWillReceiveProps(newProps: TagContainerProps) {
-        this.fetchTags(newProps.mxObject);
+        this.fetchData(newProps.mxObject);
         this.resetSubscriptions(newProps.mxObject);
     }
 
@@ -94,65 +96,48 @@ export default class TagContainer extends Component<TagContainerProps, TagContai
         this.subscriptionHandles.forEach(mx.data.unsubscribe);
     }
 
-    // private isReadOnly() {
-    //     const { booleanAttribute, editable, mxObject, readOnly } = this.props;
-    //     if (editable === "default" && mxObject) {
-    //         return readOnly || mxObject.isReadonlyAttr(booleanAttribute);
-    //     }
+    private isReadOnly = (): boolean => {
+        return !this.props.mxObject || this.props.editable === "never" ||
+            this.props.mxObject.isReadonlyAttr(this.reference);
+    }
 
-    //     return true;
-    // }
-
-    private isReadOnly() {
-        const { editable, mxObject, readOnly } = this.props;
-        if (editable === "default" && mxObject) {
-            return readOnly;
+    private handleValidations = (validations: mendix.lib.ObjectValidation[]) => {
+        const alertMessage = validations[0].getErrorReason(this.reference);
+        validations[0].removeAttribute(this.reference);
+        if (alertMessage) {
+            this.setState({ alertMessage });
         }
-
-        return true;
     }
 
     private resetSubscriptions(mxObject?: mendix.lib.MxObject) {
-        this.subscriptionHandles.forEach(window.mx.data.unsubscribe);
+        if (this.subscriptionHandles.length > 0) {
+            this.subscriptionHandles.forEach(window.mx.data.unsubscribe);
+        }
 
         if (mxObject) {
-            this.subscriptionHandles.push(mx.data.subscribe({
-                callback: guid => {
-                    mx.data.get({
-                        callback: object => this.fetchTags(object),
-                        guid: guid.toString()
-                    });
-                },
-                guid: mxObject.getGuid()
-            }));
-
-            this.subscriptionHandles.push(mx.data.subscribe({
-                attr: this.referenceAttribute,
-                callback: guid => {
-                    mx.data.get({
-                        callback: object => this.fetchTags(object),
-                        guid: guid.toString()
-                    });
-                },
-                guid: mxObject.getGuid()
-            }));
-
-            this.subscriptionHandles.push(mx.data.subscribe({
-                callback: validations => {
-                    window.mx.ui.error(validations[0].getErrorReason(this.referenceAttribute));
-                },
+            this.subscriptionHandles.push(window.mx.data.subscribe({
+                callback: () => this.handleValidations,
                 guid: mxObject.getGuid(),
                 val: true
+            }));
+            this.subscriptionHandles.push(window.mx.data.subscribe({
+                callback: () => this.fetchData(mxObject),
+                guid: mxObject.getGuid()
+            }));
+            this.subscriptionHandles.push(window.mx.data.subscribe({
+                attr: this.reference,
+                callback: () => this.fetchData(mxObject),
+                guid: mxObject.getGuid()
             }));
         }
     }
 
-    private lazyLoadSuggestions() {
-        this.fetchTags(this.props.mxObject);
+    private lazyLoadSuggestions = () => {
+        this.fetchData(this.props.mxObject);
         this.setState({ lazyLoaded: true });
     }
 
-    private fetchTags(mxObject: mendix.lib.MxObject) {
+    private fetchData(mxObject: mendix.lib.MxObject) {
         if (!this.state.fetchTags) {
             const { tagConstraint } = this.props;
             const constraint = tagConstraint
@@ -170,13 +155,13 @@ export default class TagContainer extends Component<TagContainerProps, TagContai
         }
     }
 
-    private processTags(tagData: mendix.lib.MxObject[]) {
-        const referenceTags = this.props.mxObject.getReferences(this.referenceAttribute) as string[];
+    private processTags = (tagData: mendix.lib.MxObject[]) => {
+        const referenceTags = this.props.mxObject.getReferences(this.reference) as string[];
         const getSuggestions = tagData.map(object => ({ value: object.get(this.props.tagAttribute) as string }));
         const currentTags = this.getCurrentTags(tagData, referenceTags);
-        const getTags = currentTags !== []
-        ? currentTags.map(object => ({ value: object.get(this.props.tagAttribute) as string }))
-        : [];
+        const getTags = currentTags && currentTags.length > 0
+            ? currentTags.map(object => ({ value: object ? object.get(this.props.tagAttribute) as string : "" }))
+            : [];
 
         this.setState({
             fetchTags: true,
@@ -186,51 +171,49 @@ export default class TagContainer extends Component<TagContainerProps, TagContai
         });
     }
 
-    private getCurrentTags(objects: mendix.lib.MxObject[], availableTags: string[]): mendix.lib.MxObject[] {
-        const currentTags: mendix.lib.MxObject[] = [];
-
-        objects.map(object => {
-            if (availableTags.length > 0) {
-                availableTags.forEach(tagGuid => {
-                    if (tagGuid === object.getGuid()) {
-                        currentTags.push(object);
-                    }
-                });
-            }
-        });
-
-        return currentTags;
+    private getCurrentTags(MxObjects: mendix.lib.MxObject[], availableTags: string[]): (mendix.lib.MxObject | undefined)[] | undefined {
+        if (availableTags.length > 0) {
+            return availableTags.map(guid => {
+                return MxObjects.find(object =>
+                    guid === object.getGuid()
+                );
+            });
+        }
     }
 
-    private createTag(newTag: string) {
-        const { afterCreateMicroflow, onChangeMicroflow, mxObject } = this.props;
+    private validateTag = (newTag: string) => {
+        const { mxObject } = this.props;
         const tagList = this.state.tagList;
 
-        // Check if newTag exists in the database
+        // Compare new tag with the existing tags
         for (const object of this.state.tagCache) {
-            const existingTag = object.get(this.props.tagAttribute) as string;
-            if (newTag !== "" && newTag === existingTag) {
+            const isExisting = object.get(this.props.tagAttribute) as string;
+            if (newTag !== "" && newTag === isExisting) {
                 tagList.push(newTag);
                 this.setState({ tagList });
                 if (!mxObject.isReference(object.getGuid())) {
-                    mxObject.addReference(this.referenceAttribute, object.getGuid());
+                    mxObject.addReference(this.reference, object.getGuid());
                     this.saveChanges(mxObject);
                 }
 
                 return;
             }
         }
+
+        this.createTag(mxObject, newTag);
+        tagList.push(newTag);
+        this.setState({ tagList });
+    }
+
+    private createTag = (mxObject: mendix.lib.MxObject, tag: string) => {
         mx.data.create({
             callback: object => {
-                object.set(this.props.tagAttribute, newTag);
+                object.set(this.props.tagAttribute, tag);
                 mx.data.commit({
                     callback: () => {
-                        mxObject.addReference(this.referenceAttribute, object.getGuid());
+                        mxObject.addReference(this.reference, object.getGuid());
                         this.saveChanges(mxObject);
-                        if (afterCreateMicroflow || onChangeMicroflow) {
-                            this.executeAction(mxObject, afterCreateMicroflow);
-                            this.executeAction(mxObject, onChangeMicroflow);
-                        }
+                        this.executeAction(mxObject);
                     },
                     error: error => window.mx.ui.error("Error occurred attempting to commit: " + error.message),
                     mxobj: object
@@ -239,12 +222,9 @@ export default class TagContainer extends Component<TagContainerProps, TagContai
             entity: this.tagEntity,
             error: error => window.mx.ui.error(`Error creating tag object ${this.tagEntity}, ${error.message}`)
         });
-
-        tagList.push(newTag);
-        this.setState({ tagList });
     }
 
-    private removeTag(value: string) {
+    private removeTag = (value: string) => {
         if (value) {
             const tagIndex = this.state.tagList.indexOf(value);
             const tagList = this.state.tagList.slice();
@@ -260,36 +240,58 @@ export default class TagContainer extends Component<TagContainerProps, TagContai
     }
 
     private removeReference(tag: string) {
-        const { onChangeMicroflow, mxObject } = this.props;
+        const { mxObject } = this.props;
         const xpath = `//${this.tagEntity}[ ${this.props.tagAttribute} = '${tag}' ]`;
-
         mx.data.get({
             callback: (object) => {
-                mxObject.removeReferences(this.referenceAttribute, [ object[0].getGuid() ]);
+                mxObject.removeReferences(this.reference, [ object[0].getGuid() ]);
                 this.saveChanges(mxObject);
-                if (onChangeMicroflow) {
-                    this.executeAction(mxObject, onChangeMicroflow);
-                }
+                this.executeAction(mxObject);
             },
             error: error => `${error.message}`,
             xpath
         });
     }
 
-    private saveChanges(object: mendix.lib.MxObject) {
-        mx.data.commit({ mxobj: object, callback: () => null });
+    private saveChanges(mxobj: mendix.lib.MxObject) {
+        mx.data.commit({ mxobj, callback: () => null });
     }
 
-    private executeAction(mxObject: mendix.lib.MxObject, action?: string) {
-        if (action) {
-            window.mx.ui.action(action, {
-                error: error => window.mx.ui.error(`Error while executing microflow: ${action}: ${error.message}`),
-                origin: this.props.mxform,
-                params: {
-                    applyto: "selection",
-                    guids: [ mxObject.getGuid() ]
-                }
-            });
+    private executeAction = (mxObject: mendix.lib.MxObject) => {
+        const { afterCreateMicroflow, afterCreateNanoflow, onChangeMicroflow, onChangeNanoflow } = this.props;
+        const context = new mendix.lib.MxContext();
+        context.setContext(mxObject.getEntity(), mxObject.getGuid());
+
+        if (afterCreateMicroflow) {
+            this.callMicroflow(context, afterCreateMicroflow);
         }
+        if (onChangeMicroflow) {
+            this.callMicroflow(context, onChangeMicroflow);
+        }
+        if (afterCreateNanoflow && afterCreateNanoflow.nanoflow) {
+            this.callNanoflow(context, afterCreateNanoflow);
+        }
+        if (onChangeNanoflow && onChangeNanoflow.nanoflow) {
+            this.callNanoflow(context, onChangeNanoflow);
+        }
+    }
+
+    private callNanoflow = (context: mendix.lib.MxContext, nanoflow: Nanoflow) => {
+        window.mx.data.callNanoflow({
+            context,
+            error: error => window.mx.ui.error(
+                `An error occurred while executing the on change nanoflow: ${error.message}`
+            ),
+            nanoflow,
+            origin: this.props.mxform
+        });
+    }
+
+    private callMicroflow = (context: mendix.lib.MxContext, action: string) => {
+        window.mx.ui.action(action as string, {
+            context,
+            error: error => window.mx.ui.error(`Error while executing microflow: ${action}: ${error.message}`),
+            origin: this.props.mxform
+        });
     }
 }
